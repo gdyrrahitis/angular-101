e35ee51dd4266bf8da23d31bb3b5cf637a589bcd
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2016-05-02 11:35:26 +0100

    Added directive component

--- modules/Sections/Section2/101_2.1_Directives/app/directives.component.ts
+++ modules/Sections/Section2/101_2.1_Directives/app/directives.component.ts
@@ -1,9 +1,22 @@
 import {Component} from "angular2/core";
+import {PokemonRenderDirective} from "./directives/pokeRender.directive";
 
 @Component({
     selector: "directives",
-    templateUrl: "modules/Sections/Section2/101_2.1_Directives/templates/directives.component.html"
+    templateUrl: "modules/Sections/Section2/101_2.1_Directives/templates/directives.component.html",
+    directives: [PokemonRenderDirective]
 })
 export class DirectivesComponent {
+    private pokemonName: string;
+    private pokemonNextEvolutionName: string;
+    clicks = 0;
     
+    constructor() {
+        this.pokemonName = "mankey";
+        this.pokemonNextEvolutionName = "primeape";
+    }
+    
+    myOnClickHandler (event) {
+        this.clicks = event.value;
+    }
 }
