80fcf9944ed27ca35305a80643cb083fddef9550
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2017-03-03 00:16:34 +0000

    Added style in row for margin bottom 15px

--- src/samples/Routing/Child/src/components/child.component.ts
+++ src/samples/Routing/Child/src/components/child.component.ts
@@ -3,7 +3,12 @@ import { Component } from "@angular/core";
 @Component({
     moduleId: module.id,
     selector: "app-child",
-    templateUrl: "./child.component.html"
+    templateUrl: "./child.component.html",
+    styles: [`
+        .margin-bottom-15{
+            margin-bottom: 15px;
+        }
+    `]
 })
 export class ChildComponent {
 
