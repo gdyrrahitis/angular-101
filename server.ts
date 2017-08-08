ee8b0d33c4d0f89f5a04cdc464de54074be016b1
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2017-08-08 22:56:20 +0100

    Updated configuration [ci skip]

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import * as path from "path";
 import http = require("http");
 import * as fs from "fs";
 
-const port: number =process.env.PORT || 8080;
+const port: number = process.env.PORT || 8080;
 const env: string = process.env.NODE_ENV || "development";
 let app: express.Application = express();
 app.use(express.static(__dirname));
