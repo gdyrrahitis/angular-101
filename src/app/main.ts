91ae28bdfe18d4573d545cbbc7a429f99aad44fd
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2018-07-22 12:39:58 +0100

    Updated environment

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,9 +3,9 @@ import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { AppModule } from "./app.module";
 
 // tslint:disable-next-line:no-var-requires
-let config: { node_env: string } = require("../../env-config.json");
-if (config.node_env.trim() === "production") {
-    enableProdMode();
-}
+// let config: { node_env: string } = require("../../env-config.json");
+// if (config.node_env.trim() === "production") {
+enableProdMode();
+// }
 
 platformBrowserDynamic().bootstrapModule(AppModule).catch((error) => { console.error(error); });
