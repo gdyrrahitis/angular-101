6b55f68264644c4551ae60e1886d8a25707a607e
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2018-07-22 14:41:55 +0100

    Updated express server

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import * as path from "path";
 import http = require("http");
 import * as fs from "fs";
 
-const port = process.env.PORT;//) || 8080;
+const port = process.env.PORT || 8484;
 const env: string = process.env.NODE_ENV || "development";
 let app: express.Application = express();
 app.use(express.static(__dirname));
@@ -11,16 +11,12 @@ app.use("/src", express.static("src"));
 app.use("/node_modules", express.static("node_modules"));
 app.get("*", (request, response, next) => {
     response.sendFile("index.html", { root: __dirname });
-    // if (request.method === 'GET' && request.accepts('html') 
-    //     && !request.is('json') && !request.path.includes('.')) {
-    //         response.sendFile('index.html', { oot: __dirname })
-    //   } else next()
 });
 
-// let contents = fs.readFileSync("env-config.json", "utf8");
-// let config = JSON.parse(contents);
-// config.node_env = env;
-// let json = JSON.stringify(config);
-// fs.writeFileSync("env-config.json", json, { encoding: "utf8" });
+let contents = fs.readFileSync("env-config.json", "utf8");
+let config = JSON.parse(contents);
+config.node_env = env;
+let json = JSON.stringify(config);
+fs.writeFileSync("env-config.json", json, { encoding: "utf8" });
 
 http.createServer(<any>app).listen(port);
